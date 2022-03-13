575c7da71fe68d249975c1fd9d41a7db310aee75
MasonBaek <82987683+MasonBaek@users.noreply.github.com>
2022-03-14 08:45:52 +0900

    Update Service

--- mrsbok/src/main/java/com/lunch/mrsbok/service/LunchService.java
+++ mrsbok/src/main/java/com/lunch/mrsbok/service/LunchService.java
@@ -1,13 +1,19 @@
-package com.lunch.mrsbok.service;
-import com.lunch.mrsbok.domain.StorePickedVO;
+package com.lunch.mason.service;
+import com.lunch.mason.domain.*;
+
 import java.util.List;
 public interface LunchService {
+	
 	// 식당 전체 리스트
-	List<String> getStoreList() throws Exception;
+	List<StoreVO> getStoreList() throws Exception;
 	// 사용자가 선택한 식당 DB에 넣기(기존에 선택한식당이 있으면 변경하기)
 	void choiceStore(String id, StorePickedVO storePickedVO);
 	// //전체 유저의 식당,사무실 리스트 가져오기
-	List checkStoreListBok(String today);
-	List checkStoreListThe(String today);
+	List getStoreListOfficeA(String today);
+	List getStoreListOfficeB(String today);
+	
+	List<MyOrderListVO> getMyOrderlist(String id, String today);
+	
+	List<ChoiceMenuVO> getOrderList(String today, String office);
 	
 }
--- mrsbok/src/main/java/com/lunch/mrsbok/service/LunchServiceImpl.java
+++ mrsbok/src/main/java/com/lunch/mrsbok/service/LunchServiceImpl.java
@@ -1,37 +1,34 @@
-package com.lunch.mrsbok.service;
-import com.lunch.mrsbok.domain.StorePickedListVO;
-import com.lunch.mrsbok.domain.StorePickedVO;
-import com.lunch.mrsbok.mapper.LunchMapperInterface;
+package com.lunch.mason.service;
+
+import com.lunch.mason.domain.*;
+import com.lunch.mason.mapper.LunchMapperInterface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 @Service
 public class LunchServiceImpl implements LunchService {
 	
 	Logger logger = LoggerFactory.getLogger(LunchServiceImpl.class);
 	private final LunchMapperInterface lunchMapperInterface;
-	
 	public LunchServiceImpl(LunchMapperInterface lunchMapperInterface) {
 		this.lunchMapperInterface = lunchMapperInterface;
 	}
-	
+	public String today() {
+		return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd"));
+	}
 	// 식당 리스트 가져오기
 	@Override
-	public List<String> getStoreList() throws Exception {
+	public List<StoreVO> getStoreList() throws Exception {
 		logger.info(" Service -> getStoreList start");
-		int maxIndex = lunchMapperInterface.getMaxStoreNum();
-		logger.info("maxIndex -> " +maxIndex);
-		List<String> list = new ArrayList<>();
-		int storeNum = 0;
-		while (storeNum<maxIndex){
-			list.add(lunchMapperInterface.getStore(storeNum));
-			storeNum++;
-		}
+		List<StoreVO> list = new ArrayList<>();
+		// 식당전체 다들고오기
+		list.addAll(lunchMapperInterface.getStore());
+		//
 		logger.info("service -> list : "+list);
 		return list;
 	}
@@ -40,36 +37,46 @@ public class LunchServiceImpl implements LunchService {
 	@Override
 	public void choiceStore(String id, StorePickedVO storePickedVO) {
 		logger.info(" Service -> choiceStore() start");
-		String today = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd")); // 01-04 양식으로 날짜 String 변수에 넣음
-		logger.info(" Service -> today : '"+ today+"'");
+		logger.info(" Service -> today : '"+ today()+"'");
 		String office = storePickedVO.getOffice();
 		String storePicked = storePickedVO.getStorePicked();
-		if(lunchMapperInterface.checkStore(id, today)){ // 있으머는 어~~~업데이트
+		if(lunchMapperInterface.checkStore(id, today())){ // 있으머는 어~~~업데이트
 			logger.info("업데이트");
-			lunchMapperInterface.updateChoiceStore(id, today, storePicked, office);
+			lunchMapperInterface.updateChoiceStore(id, today(), storePicked, office);
 		}else{ // 없으면 인사트
-			int seq = lunchMapperInterface.getDateSeq(today) + 1;
+			int seq = lunchMapperInterface.getDateSeq(today()) + 1;
 			logger.info("인써트");
-			lunchMapperInterface.choiceStore(id, today, storePicked, office,seq);
+			lunchMapperInterface.choiceStore(id, today(), storePicked, office,seq);
 		}
-		logger.info(" Service -> choiceStore() start");
+		logger.info(" Service -> choiceStore() end");
 	}
 	
 	
 	//전체 유저의 식당,사무실 리스트 가져오기
 	@Override
-	public List checkStoreListBok(String today) {
+	public List getStoreListOfficeA(String today) {
 		List<StorePickedListVO> list = new ArrayList<>();
-		list.addAll(lunchMapperInterface.getStoreListBok(today));
-		logger.info("\n Service -> checkStoreListBok -> list : "+ list);
+		list.addAll(lunchMapperInterface.getStoreListOfficeA(today));
+		logger.info("\n Service -> getStoreListOfficeA -> list : "+ list);
 		return list;
 	}
 	//전체 유저의 식당,사무실 리스트 가져오기
 	@Override
-	public List checkStoreListThe(String today) {
+	public List getStoreListOfficeB(String today) {
 		List<StorePickedListVO> list = new ArrayList<>();
-		list.addAll(lunchMapperInterface.getStoreListThe(today));
-		logger.info("\n Service -> checkStoreListThe-> list : "+ list);
+		list.addAll(lunchMapperInterface.getStoreListOfficeB(today));
 		return list;
 	}
+	
+	
+	@Override
+	public List<MyOrderListVO> getMyOrderlist(String id, String today) {
+		return lunchMapperInterface.getMyOrderList(id, today);
+	}
+	
+	@Override
+	public List<ChoiceMenuVO> getOrderList(String today, String office) {
+		return lunchMapperInterface.getOrderList(today, office);
+	}
+	
 }
